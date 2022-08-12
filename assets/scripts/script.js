cc5c3d8b184195423be3f83f48d55b2a7828cd76
alex <alexandry.augustin@gmail.com>
2022-08-12 13:55:00 +0100

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,8 @@
+//import 'bootstrap';
+//import '../../../node_modules/bootstrap/dist/js/bootstrap';
+//import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
+//var boostrap = require('boostrap')
+
 //var uid=0;
 
 /*window.onload = function()
